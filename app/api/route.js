147feab5cf2ef2112522850b80ed5e7964f54051
Stalin-dev <stalinlopezm@gmail.com>
2026-09-14 14:45:36 -0500

    Recogniz folders

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,6 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { syncUserCreation } from "../../../inngest/functions";
-
+import { inngest } from "@/inngest/client";
+import { syncUserCreation } from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
