8d03c8003c00d00f483b1df0feb9cab744392748
Stalin-dev <stalinlopezm@gmail.com>
2026-09-14 15:16:27 -0500

    Update inngest fucntions

--- inngest/functions.js
+++ inngest/functions.js
@@ -22,3 +22,31 @@ export const syncUserCreation = inngest.createFunction(
   }
 )
 
+// Inngest Function to update user data in database
+export const syncUserUpdation = inngest.createFunction(
+    {id: 'sync-user-update'},
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        const { data } = event
+        await prisma.user.update({
+            where: {id: data.id,},
+            data: {
+                email: data.email_addresses[0].email_address,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+            }
+        })
+    }
+)
+
+// Inngest Function to delete user from database
+export const syncUserDeletion = inngest.createFunction(
+    {id: 'sync-user-delete'},
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        const { data } = event
+        await prisma.user.delete({
+            where: {id: data.id,}
+        })
+    }
+)
