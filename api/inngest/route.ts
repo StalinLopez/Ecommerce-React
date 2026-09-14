1e3e25fcff4d41c7b64022f26440a87e21515d4d
Stalin-dev <stalinlopezm@gmail.com>
2026-09-14 12:05:38 -0500

    Initial commit: Full-stack ecommerce project with React Native, Node.js, Clerk, Inngest, Prisma, and Neon

---
+++ api/inngest/route.ts
@@ -0,0 +1,10 @@
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+import { syncUserCreation } from "@/inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation 
+  ],
+});
